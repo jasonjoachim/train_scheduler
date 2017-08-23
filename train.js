693d9eebf34a86ca0f2842b6e597d054e7ea122c
jasonjoachim <joachim.jason@gmail.com>
2017-08-23 00:34:12 -0500

    initial style and layout

---
+++ train.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCckvguwo45qkgPaDlpjhQCYfboIiO8-p4",
+    authDomain: "train-scheduler-405da.firebaseapp.com",
+    databaseURL: "https://train-scheduler-405da.firebaseio.com",
+    projectId: "train-scheduler-405da",
+    storageBucket: "",
+    messagingSenderId: "867187902694"
+  };
+  firebase.initializeApp(config);
